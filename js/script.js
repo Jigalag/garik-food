5e38d5cfa2a9d003271a19cc8d69db0ddbb8653b
Jigalag <funkylex@yandex.ru>
2018-05-06 23:15:18 +0300

    pages

--- js/script.js
+++ js/script.js
@@ -4,21 +4,93 @@
 
 $(document).ready(function() {
 
-    if ($('.slick-carousel').length > 0) {
-        $('.slick-carousel').slick({
-            arrows: true,
-            slidesToShow: 3,
+    if ($('.headerSlider').length > 0) {
+        $('.headerSlider').slick({
+            arrows: false,
+            slidesToShow: 1,
             slidesToScroll: 1,
             infinite: true,
+            dots: true,
+            autoplay: false
+        });
+    }
+
+    if ($('.complexItem').length > 0) {
+        var ps1 = new PerfectScrollbar('#firstStore');
+        var ps2 = new PerfectScrollbar('#secondStore');
+        setTimeout(function () {
+            ps1.update();
+            ps2.update();
+        }, 100);
+    }
+
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+    if ($('.promoSlider').length > 0) {
+        $('.promoSlider').slick({
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '480px',
+            infinite: true,
+            prevArrow: '<div class="slick-prev"><button type="button">Назад</button></div>',
+            nextArrow: '<div class="slick-next"><button type="button">Вперед</button></div>',
             dots: false,
             autoplay: false,
-            centerMode: true,
-            centerPadding: '30px',
             responsive: [
                 {
-                    breakpoint: 740,
+                    breakpoint: 1700,
                     settings: {
-                        slidesToShow: 1
+                        centerPadding: '350px',
+                    }
+                },
+                {
+                    breakpoint: 1550,
+                    settings: {
+                        centerPadding: '300px',
+                    }
+                },
+                {
+                    breakpoint: 1420,
+                    settings: {
+                        centerPadding: '250px',
+                    }
+                },
+                {
+                    breakpoint: 1320,
+                    settings: {
+                        centerPadding: '200px',
+                    }
+                },
+                {
+                    breakpoint: 1220,
+                    settings: {
+                        centerPadding: '150px',
+                    }
+                },
+                {
+                    breakpoint: 1120,
+                    settings: {
+                        centerPadding: '100px',
+                    }
+                },
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        centerPadding: '100px',
                     }
                 }
             ]
